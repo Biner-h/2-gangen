7a0acb42b866ec2e1012b42e3f2f5555e9f8e199
Biner-h <Binerhabib@gmail.com>
2023-09-05 14:16:56 +0200

    update

---
+++ script.js
@@ -0,0 +1,6 @@
+function multiplication() {
+    let stopValue = 20;
+    for (counter = 2; counter <= stopValue; counter += 2) {
+        document.getElementById("nummer").innerHTML += `<p>` + counter + `</p>`;
+    }
+}
